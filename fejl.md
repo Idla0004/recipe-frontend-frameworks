f2d69c3ecd3e92990bd0776c702d6c6edd45d029
Ida <idla0004@stud.kea.dk>
2026-04-26 16:13:10 +0200

    Recipe opgave igang

---
+++ fejl.md
@@ -0,0 +1,7 @@
+Opret en Markdown-fil (med endelsen .md, som du kender fra f.eks. README.md), hvor du: Identificere relevante fejlscenarier, fx:
+
+Manglende input
+For kort tekst
+Forkert datatype
+
+---
--- src/app/page.js
+++ src/app/page.js
@@ -2,64 +2,182 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+    <div className="flex flex-col justify-start my-3 mx-auto">
+      <section className="my-3">
+        <h1 className="text-3xl text-cyan-800 font-bold italic">
+          Upload your own recipe!
+        </h1>
+      </section>
+      <section className="grid grid-cols-2 bg-(--section-two-bg) text-foreground px-10 max-w-[1400px] rounded-[15px] shadow-[7px_9px_4px_rgba(0,0,0,0.25)]">
+        <div className="flex flex-col justify-center my-10">
+          <form>
+            <input
+              className="text-xl px-1 py-1 mb-5 text-start border-b border-foreground outline-none text-foreground"
+              type="text"
+              placeholder="Name your recipe"
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          </form>
+          <div className="flex gap-10 justify-between mb-4">
+            <form>
+              <input
+                type="text"
+                name="PrepTime"
+                placeholder="add preptime.. e.g 1h"
+                className="outline-none border-b border-foreground text-center"
+              />
+            </form>
+            <form>
+              <input
+                type="text"
+                name="CookingTime"
+                placeholder="add cookingtime.. e.g 30min"
+                className="outline-none border-b border-foreground text-center w-50"
+              />
+            </form>
+            <form>
+              <input
+                type="text"
+                name="Servings"
+                placeholder="Servings.. e.g 2"
+                className="outline-none border-b border-foreground text-center"
+              />
+            </form>
+            <form>
+              <input
+                type="text"
+                name="Mealtype"
+                placeholder="Mealtype.. e.g Italian"
+                className="outline-none border-b border-foreground text-center"
+              />
+            </form>
+          </div>
+          <div className="flex flex-col bg-(--section-three-bg) px-5 py-5 rounded-md">
+            <label className="text-lg text-background font-semibold mb-3">
+              Ingredients:
+            </label>
+            <div className="flex gap-3 mb-3">
+              <input
+                type="checkbox"
+                name="ingredient1checkbox"
+              />
+              <input
+                type="text"
+                name="ingredient1Name"
+                placeholder="Ingredient 1"
+                className="outline-none border-b-2 border-background text-background"
+              />
+            </div>
+            <div className="flex gap-3 mb-3">
+              <input
+                type="checkbox"
+                name="ingredient2checkbox"
+              />
+              <input
+                type="text"
+                name="ingredient2Name"
+                placeholder="Ingredient 2"
+                className="outline-none border-b-2 border-background text-background"
+              />
+            </div>
+            <div className="flex gap-3 mb-3">
+              <input
+                type="checkbox"
+                name="ingredient3checkbox"
+              />
+              <input
+                type="text"
+                name="ingredient3Name"
+                placeholder="Ingredient 3"
+                className="outline-none border-b-2 border-background text-background"
+              />
+            </div>
+            <div className="flex gap-3 mb-3">
+              <input
+                type="checkbox"
+                name="ingredient4checkbox"
+              />
+              <input
+                type="text"
+                name="ingredient4Name"
+                placeholder="Ingredient 4"
+                className="outline-none border-b-2 border-background text-background"
+              />
+            </div>
+            <div className="flex gap-3 mb-3">
+              <input
+                type="checkbox"
+                name="ingredient5checkbox"
+              />
+              <input
+                type="text"
+                name="ingredient5Name"
+                placeholder="Ingredient 5"
+                className="outline-none border-b-2 border-background text-background"
+              />
+            </div>
+            <div className="flex gap-3 mb-3">
+              <input
+                type="checkbox"
+                name="ingredient6checkbox"
+              />
+              <input
+                type="text"
+                name="ingredient6Name"
+                placeholder="Ingredient 6"
+                className="outline-none border-b-2 border-background text-background"
+              />
+            </div>
+            <div className="flex gap-3 mb-3">
+              <input
+                type="checkbox"
+                name="ingredient7checkbox"
+              />
+              <input
+                type="text"
+                name="ingredient7Name"
+                placeholder="Ingredient 7"
+                className="outline-none border-b-2 border-background text-background"
+              />
+            </div>
+            <div className="flex gap-3 mb-3">
+              <input
+                type="checkbox"
+                name="ingredient8checkbox"
+              />
+              <input
+                type="text"
+                name="ingredient8Name"
+                placeholder="Ingredient 8"
+                className="outline-none border-b-2 border-background text-background"
+              />
+            </div>
+            <div className="flex gap-3 mb-3">
+              <input
+                type="checkbox"
+                name="ingredient9checkbox"
+              />
+              <input
+                type="text"
+                name="ingredient9Name"
+                placeholder="Ingredient 9"
+                className="outline-none border-b-2 border-background text-background"
+              />
+            </div>
+            <div className="flex gap-3 mb-3">
+              <input
+                type="checkbox"
+                name="ingredient10checkbox"
+              />
+              <input
+                type="text"
+                name="ingredient10Name"
+                placeholder="Ingredient 10"
+                className="outline-none border-b-2 border-background text-background"
+              />
+            </div>
+          </div>
         </div>
-      </main>
+      </section>
     </div>
   );
 }
